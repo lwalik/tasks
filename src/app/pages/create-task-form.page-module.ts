75a496c53de45d0ddcd3d6c22ed6f89216c7e28b
Lukasz W <l.walicki1@gmail.com>
2022-03-24 18:23:49 +0100

    use createTaskFormComponent in createTaskFormPage

--- src/app/pages/create-task-form.page-module.ts
+++ src/app/pages/create-task-form.page-module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CreateTaskFormPage } from './create-task-form.page';
+import { CreateTaskFormComponentModule } from '@tasks';
 
 @NgModule({
   imports: [
     CommonModule,
+    CreateTaskFormComponentModule,
     RouterModule.forChild([
       {
         path: '',
