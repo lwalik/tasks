3c0e81df47d744b36cb24e05351ba075dcff2b60
Lukasz W <l.walicki1@gmail.com>
2022-03-26 12:35:40 +0100

    implement firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'tasks-c40b3',
+    appId: '1:662809051623:web:e60813be481a7de5d7bd42',
+    storageBucket: 'tasks-c40b3.appspot.com',
+    locationId: 'europe-west2',
+    apiKey: 'AIzaSyAN0LOdStXr0GdyIuwoTPXyHmgkrQ8xPZo',
+    authDomain: 'tasks-c40b3.firebaseapp.com',
+    messagingSenderId: '662809051623',
+  },
   production: true
 };
