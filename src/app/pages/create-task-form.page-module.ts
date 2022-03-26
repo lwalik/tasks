139e44cf734b6bb95c076025baa1c4c8f239e33a
Lukasz W <l.walicki1@gmail.com>
2022-03-26 16:21:03 +0100

    good layout version createTaskFormComponent

--- src/app/pages/create-task-form.page-module.ts
+++ src/app/pages/create-task-form.page-module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CreateTaskFormPage } from './create-task-form.page';
-import { CreateTaskFormComponentModule } from '@tasks';
+import {
+  CreateTaskFormComponentModule,
+  CurrentDateComponentModule,
+  FooterComponentModule,
+} from '@tasks';
 
 @NgModule({
   imports: [
     CommonModule,
     CreateTaskFormComponentModule,
+    CurrentDateComponentModule,
+    FooterComponentModule,
     RouterModule.forChild([
       {
         path: '',
