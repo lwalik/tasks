b5460a98b14752df919df5ec0c4bd8d3bd46b27b
Lukasz W <l.walicki1@gmail.com>
2022-03-26 19:25:08 +0100

    add routerLink in taskListComponent and createTaskFormComponent

--- projects/tasks/src/lib/adapters/primary/ui/tasks-list.component-module.ts
+++ projects/tasks/src/lib/adapters/primary/ui/tasks-list.component-module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksListComponent } from './tasks-list.component';
+import { RouterModule } from '@angular/router';
 
-@NgModule({ imports: [CommonModule],
-  	declarations: [TasksListComponent],
-  	providers: [],
-  	exports: [TasksListComponent] })
-export class TasksListComponentModule {
-}
+@NgModule({
+  imports: [CommonModule, RouterModule],
+  declarations: [TasksListComponent],
+  providers: [],
+  exports: [TasksListComponent],
+})
+export class TasksListComponentModule {}
