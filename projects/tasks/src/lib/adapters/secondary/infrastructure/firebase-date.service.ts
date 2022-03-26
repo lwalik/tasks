7c8522c5c3b6f956217e080854cad70216236b22
Lukasz W <l.walicki1@gmail.com>
2022-03-26 18:20:54 +0100

    create and use dateService

---
+++ projects/tasks/src/lib/adapters/secondary/infrastructure/firebase-date.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { GetsAllDateDtoPort } from '../../../application/ports/secondary/gets-all-date.dto-port';
+import { DateDTO } from '../../../application/ports/secondary/date.dto';
+
+@Injectable()
+export class FirebaseDateService implements GetsAllDateDtoPort {
+  getAll(): Observable<DateDTO> {
+    return of({
+      currentDate: new Date(),
+    });
+  }
+}
---
+++ projects/tasks/src/lib/application/ports/secondary/date.dto.ts
@@ -0,0 +1,3 @@
+export interface DateDTO {
+  readonly currentDate: Date;
+}
